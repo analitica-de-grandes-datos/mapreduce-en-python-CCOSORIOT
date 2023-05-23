4b327de395aaa30ad9a14cf266a340d096d5151e
CCOSORIOT <127028350+CCOSORIOT@users.noreply.github.com>
2023-05-23 08:51:51 -0500

    Pregunta 4
    Versión 4

--- pregunta_04/mapper.py
+++ pregunta_04/mapper.py
@@ -1,3 +1,9 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+    for linea in sys.stdin:
+        columnas = linea.strip().split('   ')
+        columnas2 = columnas[0]
+        sys.stdout.write("{}\t1\n".format(columnas2))
