e6b5c262b1e25a71b36b08e7fbbf42dc08a0d9a5
CCOSORIOT <127028350+CCOSORIOT@users.noreply.github.com>
2023-05-23 08:39:03 -0500

    Pregunta 3
    Versión 1

--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -1,3 +1,16 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+    atributo = {}
+    for linea in sys.stdin:
+        columnas = linea.split("\t")
+        if len(columnas) == 2:
+            columna1 = columnas[0] 
+            columna2 = int(columnas[1])
+            atributo[columna1] = columna2
+            
+    atributo_sort = sorted(atributo.items(), key=lambda x: x[1])
+    for atributo,valor in atributo_sort:
+        sys.stdout.write(f"{atributo},{valor}\n")
